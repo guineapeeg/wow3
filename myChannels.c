10b0de5f09382c8d47bc07c1bf7bd979ecf431ce
Guineapeeg <marymarylinux@gmail.com>
2023-07-01 23:17:40 -0700

    pass by reference??? pass by value???

--- myChannels.c
+++ myChannels.c
@@ -5,8 +5,9 @@
 #include <string.h>
 
 typedef struct{
-    int bufferSize;
-    int threadCount;
+    char* channelPath;
+    float low_pass_filter_value;
+    float amplification;
 } ThreadArgument;
 
 int buffer_size;
@@ -24,55 +25,43 @@ int number_of_input_files;
 
 int lock = 0;
 
+int bufferSize = 2;
+
 void* readFileThread(void * args){
+
     ThreadArgument *actualArguments = args;
 
-    char* length = "/home/vboxuser/Desktop/C assignments/assignment3/input/file_1.txt";
-    char result[strlen(length)];
-
-    char* fileBeginning = "/home/vboxuser/Desktop/C assignments/assignment3/input/file_";
-    char *fileToOpen = ".txt";
-
-    sprintf(result, "%s%d", fileBeginning, actualArguments->threadCount);
-
-    strcat(result, fileToOpen);
-
-    printf("%s", result);
-
-    
-
 //////////////////////////////////
-    // FILE *fptr;
-    // fptr = fopen(result, "r");
+    FILE *fptr;
+    fptr = fopen(actualArguments->channelPath, "r");
+    //fptr = fopen("input/file_1.txt", "r");
 
-    // int bufferSize = actualArguments->bufferSize;
-    // int resRead;
-    // char buffer[bufferSize];
-    // char c;
-
+    printf("This: %s", actualArguments->channelPath);
     
+    int resRead;
+    char buffer[bufferSize];
+    char c;
 
-    // if (fptr != NULL){
+    if (fptr != NULL){
+        do{
+            for(int i = 0; i<bufferSize && c != EOF; i++){
+            c = fgetc(fptr);
+            buffer[i] = c;
+        }
+    }while(c != EOF);
+    }else{
+        printf("Error \n");
+    }
 
-    //     do{
+    printf("%s", buffer);
 
-    //         for(int i = 0; i<bufferSize && c != EOF; i++){
-    //         c = fgetc(fptr);
-    //         buffer[i] = c;
 
-    //         if(buffer[i] != '\n' && buffer[i] != EOF){
+            // if(buffer[i] != '\n' && buffer[i] != EOF){
                
-    //         }
-    //         }
+            // }
+            // }
             
 
-    //     }while(c != EOF);
-
-        
-
-    // }else{
-    //     printf("not opened \n");
-    // }
 
     // int newSampleValue;
 /////////////////////////////////
@@ -88,17 +77,7 @@ void* readFileThread(void * args){
 
 int main(){
 
-    num_threads = 2;
-    //number_of_input_files = 3;
-
-
-    
-
-    // pthread_t thread_id;
-    // print("Before thread \n");
-    // pthread_create(&thread_id. NULL, threadFunction, NULL);
-    // pthread_join(thread_id, NULL);
-    // printf("After thread");
+    num_threads = 3;
 
     FILE * metadataFile;
     char *line = NULL;
@@ -109,6 +88,14 @@ int main(){
     
     number_of_input_files = atoi(line);
     printf("Number of input files: %d", number_of_input_files);
+    //each thread works on p files
+
+    int p = number_of_input_files / num_threads;
+
+    char test[20];
+    char fileN[20];
+    char firstNum[20];
+    char secondNum[20];
     
 
     for(int i = 1; i <= num_threads; i++){
@@ -116,22 +103,36 @@ int main(){
         pthread_t thread_id;
 
         ThreadArgument *args = malloc(sizeof *args);
-        
-        args->bufferSize = 2; 
-        args->threadCount = i;
 
-        //compare and swap
         //while(1){
-            while(__sync_bool_compare_and_swap(&lock, 0, 1) != 0);
+        while(__sync_bool_compare_and_swap(&lock, 0, 1) != 0);
             //critical section
+        
+        //read = getline(&line, &len, metadataFile);
+        // while(fgets(test, 20, metadataFile) != NULL){
+
+        // }
+
+        fscanf(metadataFile, "%s %s %s", fileN, firstNum, secondNum);
+
+        printf("Printing from %d, fileName: %s", i, fileN);
+
+        
+        // args->channelPath = *line;
+        // printf("Printing from %d: %s", i, args->channelPath);
+
+        // read = getline(&line, &len, metadataFile);
+        // args->low_pass_filter_value = atof(line);
+        // printf("Printing from %d: %d", i, args->low_pass_filter_value);
+
+        // read = getline(&line, &len, metadataFile);
+        // args->amplification = atof(line);
+        // printf("Printing from %d: %s", i, args->channelPath);
+
         // printf("Thread number %d", i);
         // pthread_create(&thread_id, NULL, readFileThread, args);
         // pthread_join(thread_id, NULL);
         // printf("After thread");
-            for (int i2 = 0; i2 < 3; i2++){
-                read = getline(&line, &len, metadataFile);
-                printf("Printing from %d: %s", i, line);
-            }
 
 
             //
@@ -143,19 +144,7 @@ int main(){
         free(args);
     }
 
-    //char myString[100];
-    //fgets(myString, 100, fptr);
-
-    // printf("%s", myString);
-
-    //fclose(fpMetadata);
-
-    //printf("hello");
-
     return 0;
-
-
-
 
 
 }
